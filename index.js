c042b85c224feeaf6c2a0a449d699d604c742438
Narayut <narayutput@gmail.com>
2016-02-14 11:25:50 +0700

    Test
    Test

---
+++ index.js
@@ -0,0 +1,30 @@
+/* โหลด Express มาใช้งาน */
+var dt = require('./decision');
+var app = require('express')();
+var bodyParser = require('body-parser');
+
+// parse application/json 
+app.use(bodyParser.json())
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// define port
+var port = process.env.PORT || 7777;
+
+app.get('/decision/:temp/:humid/:pressure/:wind', function (req, res) {
+    var temp = req.params.temp;
+    var humid = req.params.humid;
+    var pressure = req.params.pressure;
+    var windspd = req.params.wind;
+    var result = {
+    	"temp" : temp 
+    }
+    res.json(dt.findAll(temp,humid,pressure,windspd));
+    //res.json(dt.findDicision(temp,humid,pressure,windspd,rain));
+});
+
+
+/* สั่งให้ server ทำการรัน Web Server ด้วย port ที่เรากำหนด */
+app.listen(port, function() {
+    console.log('Starting node.js on port ' + port);
+});
